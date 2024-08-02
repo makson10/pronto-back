f36a05b18a097f6ec5d678de124a211d43da5118
makson10 <makson94021@gmail.com>
2024-08-03 01:22:28 +0300

    feat: add presents to profile

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -35,7 +35,7 @@ export class UserService {
         data: {
           ...newUser,
           Profiles: {
-            create: {},
+            create: { presents: [] },
           },
         },
       });
