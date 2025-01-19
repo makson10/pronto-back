8aa374b3273969db4353718f2b8b709ebfa9ff47
makson10 <makson94021@gmail.com>
2025-01-19 14:30:47 +0200

    feat: add swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-import * as cookieParser from 'cookie-parser';
-import * as session from 'express-session';
-// import cookieParser from 'cookie-parser';
-// import session from 'express-session';
+// import * as cookieParser from 'cookie-parser';
+// import * as session from 'express-session';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,15 @@ async function bootstrap() {
       saveUninitialized: false,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Pronto')
+    .setDescription('Swagger documentation for pronto backend')
+    .setVersion('1.0')
+    .addTag('pronto')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(4000);
 }
